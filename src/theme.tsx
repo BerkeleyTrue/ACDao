5d5b1ec20f75437975f5798f6907d72203cc277b
Berkeley True <berkeleytrue@users.noreply.github.com>
2022-03-28 23:00:50 -0700

    fix(src/theme): initial dark mode

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,15 +1,7 @@
 import { extendTheme } from '@chakra-ui/react';
-import { createBreakpoints } from '@chakra-ui/theme-tools';
 import { withProse } from '@nikolovlazar/chakra-ui-prose';
 
 const fonts = { mono: `'Menlo', monospace` };
-
-const breakpoints = createBreakpoints({
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
-});
 
 const theme = extendTheme(
   {
@@ -17,9 +9,8 @@ const theme = extendTheme(
       black: '#16161D',
     },
     fonts,
-    breakpoints,
     config: {
-      colorMode: 'dark',
+      initialColorMode: 'dark',
     },
   },
   withProse(),
